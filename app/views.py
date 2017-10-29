d14bf257617b97fa08487193eda3c635ddc81845
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-28 23:48:42 -0500

    Added password

--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from flask import render_template, send_from_directory, request, jsonify
 from app import app
 
+import hashlib, uuid
+
 import game
+
+from app import __config__ as config
+
+def compare_password(password, correct_hash):
+    """
+    Compares password with hash
+    """
+    hashed_password = hashlib.sha512(password).hexdigest()
+    return hashed_password == correct_hash
 
 @app.route('/index', methods=['POST'])
 @app.route('/', methods=['POST'])
@@ -12,6 +23,10 @@ def index():
     json_obj = request.get_json()
     conv = json_obj["conversation_id"];
     command = json_obj["command"] if "command" in json_obj else ""
+    password = request.headers.get('X-Password')
+
+    if not compare_password(password, config.hashed_password):
+        return jsonify({"error": "401 Unauthorized: Password is invalid"}), 401
     
     if game.contains_conv(conv):
         return jsonify({"response": game.execute_command_conv(conv, command)})
