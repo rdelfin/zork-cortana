f29fe292e3b5057d7f943a5824797f3be85bdf20
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-28 19:32:52 -0500

    Implemented game creation in game.py

--- app/game.py
+++ app/game.py
@@ -1,8 +1,29 @@
+"""
+Accesses game state, and runs Zork executable to run game.
+"""
 
-conv_game_map = set()
+import subprocess
+
+CONV_GAME_MAP = set()
 
 def contains_conv(conv):
-    return conv in conv_game_map
+    """
+    Checks if a given conversation already
+    has an associated game
+    """
+    return conv in CONV_GAME_MAP
 
 def create_conv(conv):
+    """
+    Creates the game for a new conversation
+    """
+    zorkp = subprocess.Popen(['gamebin/zork', conv + '.dat'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    zorkp.communicate(input='save')[0]
+    zorkp.kill()
+    CONV_GAME_MAP |= {conv}
+
+def execute_command_conv(conv):
+    """
+    Execute a Zork command for a given conversation
+    """
     pass
