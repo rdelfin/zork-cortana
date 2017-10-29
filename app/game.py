eb16adf2816711c081a9204d78cce7e13ee66cb7
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-28 21:13:07 -0500

    Fixed game.py to process first message properly

--- app/game.py
+++ app/game.py
@@ -12,8 +12,11 @@ def filter_message(message, first=False):
     """
     Filters out uneccessary text from console output
     """
+    print("Filtering (first=%s) \"%s\"" % (str(first), message))
+
     lines = message.split('\n')[(1 if first else 5):]
-    lines[0] = lines[0][1:]
+    if not first:
+        lines[0] = lines[0][1:]
 
     stop = 0
     while stop < len(lines):
@@ -24,6 +27,7 @@ def filter_message(message, first=False):
     if stop < len(lines):
         lines = lines[:stop]
 
+    print("Filtered: \"%s\"" % '\n'.join(lines))
     return '\n'.join(lines)
 
 def contains_conv(conv):
@@ -31,26 +35,29 @@ def contains_conv(conv):
     Checks if a given conversation already
     has an associated game
     """
+    global CONV_GAME_MAP
     return conv in CONV_GAME_MAP
 
 def create_conv(conv):
     """
     Creates the game for a new conversation
     """
-    zorkp = subprocess.Popen(['gamebin/zork', path.abspath('saves/' + conv + '.dat')],
+    global CONV_GAME_MAP
+
+    zorkp = subprocess.Popen([path.abspath('gamebin/zork'), path.abspath('saves/' + conv + '.dat')],
                              cwd=path.abspath('gamebin/'),
                              stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     message = zorkp.communicate(input='save')[0]
-    zorkp.kill()
     CONV_GAME_MAP |= {conv}
 
-    return filter_message(message)
+    return filter_message(message, True)
 
 def execute_command_conv(conv, command):
     """
     Execute a Zork command for a given conversation
     """
-    zorkp = subprocess.Popen(['gamebin/zork', path.abspath('saves/' + conv + '.dat')],
+    print("Binary: %s\nSave file:%s\nCWD: %s" % (path.abspath('gamebin/zork'), path.abspath('saves/' + conv + '.dat'), path.abspath('gamebin/')))
+    zorkp = subprocess.Popen([path.abspath('gamebin/zork'), path.abspath('saves/' + conv + '.dat')],
                              cwd=path.abspath('gamebin/'),
                              stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     message = zorkp.communicate(input='restore\n' + command + '\nsave\nquit\ny\n')[0]
@@ -61,6 +68,8 @@ def finish_conv(conv):
     """
     Finish a given conversation. Will delete from file and CONV_GAME_MAP
     """
+    global CONV_GAME_MAP
+
     CONV_GAME_MAP -= {conv}
     try:
         os.remove(path.abspath('saves/' + conv + '.dat'))
